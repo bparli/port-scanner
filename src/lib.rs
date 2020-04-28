79884b67b4ff1af7b0eea3a8e6ac8ec35920ef9f
Ben Parli <ben.parli@creditkarma.com>
2020-04-27 20:26:12 -0700

    use FuturesUnordered  instead of join_all

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use async_std::io;
 use async_std::net::TcpStream;
-use async_std::prelude::*;
-use futures::future::join_all;
+use async_std::task;
+use futures::stream::{FuturesUnordered, StreamExt};
 use std::io::ErrorKind;
 use std::net::{Shutdown, SocketAddr};
 use std::time::Duration;
@@ -15,7 +15,7 @@ impl Scanner {
         Scanner { timeout: timeout }
     }
 
-    pub async fn run(&self, host: String, port_start: u16, port_end: u16) -> Vec<u16> {
+    pub async fn run(&self, host: String, port_start: u16, port_end: u16) -> Vec<SocketAddr> {
         execute(host, port_start, port_end, self.timeout).await
     }
 
@@ -25,36 +25,45 @@ impl Scanner {
         port_start: u16,
         port_end: u16,
         batch: u16,
-    ) -> Vec<u16> {
+    ) -> Vec<SocketAddr> {
         let mut begin = port_start;
         let mut end = batch;
-        let mut all_ports = Vec::new();
+        let mut all_addrs = Vec::new();
 
         while end < port_end {
-            let mut batch_ports = execute(host.clone(), begin, end, self.timeout).await;
-            all_ports.append(&mut batch_ports);
+            let mut batch_addrs = execute(host.clone(), begin, end, self.timeout).await;
+            all_addrs.append(&mut batch_addrs);
             begin = end;
             end += batch;
         }
-        all_ports
+        all_addrs
     }
 }
 
-async fn execute(host: String, port_start: u16, port_end: u16, timeout: Duration) -> Vec<u16> {
-    let mut ftrs = Vec::new();
+async fn execute(
+    host: String,
+    port_start: u16,
+    port_end: u16,
+    timeout: Duration,
+) -> Vec<SocketAddr> {
+    let mut ftrs = FuturesUnordered::new();
     for port in port_start..port_end {
         ftrs.push(try_connect(host.clone(), port, timeout));
     }
 
-    let mut ports: Vec<_> = join_all(ftrs).await;
-    ports.retain(|port| port.is_ok());
-    ports
-        .into_iter()
-        .map(|port| port.unwrap())
-        .collect::<Vec<_>>()
+    task::block_on(async {
+        let mut open_addrs: Vec<SocketAddr> = Vec::new();
+        while let Some(result) = ftrs.next().await {
+            match result {
+                Ok(addr) => open_addrs.push(addr),
+                Err(_) => {}
+            }
+        }
+        open_addrs
+    })
 }
 
-async fn try_connect(host: String, port: u16, timeout: Duration) -> io::Result<u16> {
+async fn try_connect(host: String, port: u16, timeout: Duration) -> io::Result<SocketAddr> {
     let addr = host.to_string() + ":" + &port.to_string();
     match addr.parse() {
         Ok(sock_addr) => match connect(sock_addr, timeout).await {
@@ -62,7 +71,7 @@ async fn try_connect(host: String, port: u16, timeout: Duration) -> io::Result<u
                 match stream_result.shutdown(Shutdown::Both) {
                     _ => {}
                 }
-                Ok(port)
+                Ok(sock_addr)
             }
             Err(e) => match e.kind() {
                 ErrorKind::Other => {
