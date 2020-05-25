6a47005ec0483ca275c150df4b920f373a77a04f
Ben Parli <ben.parli@creditkarma.com>
2020-05-25 16:36:44 -0700

    fix futures 0.3.6 error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use async_std::io;
 use async_std::net::TcpStream;
-use async_std::task;
 use futures::stream::{FuturesUnordered, StreamExt};
 use std::io::ErrorKind;
 use std::net::{Shutdown, SocketAddr};
@@ -33,7 +32,7 @@ impl Scanner {
         while end <= port_end {
             let mut batch_addrs = execute(host.clone(), begin, end, self.timeout).await;
             all_addrs.append(&mut batch_addrs);
-            begin = end;
+            begin = end+1;
             end += batch;
         }
         all_addrs
@@ -51,16 +50,14 @@ async fn execute(
         ftrs.push(try_connect(host.clone(), port, timeout));
     }
 
-    task::block_on(async {
-        let mut open_addrs: Vec<SocketAddr> = Vec::new();
-        while let Some(result) = ftrs.next().await {
-            match result {
-                Ok(addr) => open_addrs.push(addr),
-                Err(_) => {}
-            }
+    let mut open_addrs: Vec<SocketAddr> = Vec::new();
+    while let Some(result) = ftrs.next().await {
+        match result {
+            Ok(addr) => open_addrs.push(addr),
+            Err(_) => {}
         }
-        open_addrs
-    })
+    }
+    open_addrs
 }
 
 async fn try_connect(host: String, port: u32, timeout: Duration) -> io::Result<SocketAddr> {
@@ -101,75 +98,72 @@ mod tests {
     use futures::future::join_all;
     use std::net::SocketAddr;
     use std::time::Duration;
+    use super::Scanner;
 
     #[test]
-    fn test_run() -> io::Result<()> {
+    fn test_run()  {
+        let sock_addr1 = "127.0.0.1:8080".parse().unwrap();
+        let sock_addr2 = "127.0.0.1:8081".parse().unwrap();
+
         task::block_on(async {
-            let sock_addr1 = "127.0.0.1:8080".parse().unwrap();
-            let sock_addr2 = "127.0.0.1:8081".parse().unwrap();
-            let listener1 = TcpListener::bind(sock_addr1).await?;
-            let listener2 = TcpListener::bind(sock_addr2).await?;
+            let listener1 = TcpListener::bind(sock_addr1).await.unwrap();
+            let listener2 = TcpListener::bind(sock_addr2).await.unwrap();
             task::spawn(async move { listener1.accept().await });
             task::spawn(async move { listener2.accept().await });
+        });
 
-            use super::Scanner;
-            let ps = Scanner::new(Duration::from_secs(4));
+        let ps = Scanner::new(Duration::from_secs(4));
 
-            let ftr = ps.run("127.0.0.1".to_string(), 8000, 8100);
-            let my_addrs: Vec<SocketAddr> = task::block_on(async { ftr.await });
-            assert_eq!(my_addrs, vec![sock_addr1, sock_addr2]);
-            Ok(())
-        })
+        let ftr = ps.run("127.0.0.1".to_string(), 8000, 8100);
+        let my_addrs: Vec<SocketAddr> = task::block_on(async { ftr.await });
+        assert_eq!(my_addrs, vec![sock_addr1, sock_addr2]);
     }
 
     #[test]
-    fn test_batched() -> io::Result<()> {
+    fn test_batched() {
+        let sock_addr1 = "127.0.0.1:9080".parse().unwrap();
+        let sock_addr2 = "127.0.0.1:9081".parse().unwrap();
+
         task::block_on(async {
-            let sock_addr1 = "127.0.0.1:9080".parse().unwrap();
-            let sock_addr2 = "127.0.0.1:9081".parse().unwrap();
-            let listener1 = TcpListener::bind(sock_addr1).await?;
-            let listener2 = TcpListener::bind(sock_addr2).await?;
+            let listener1 = TcpListener::bind(sock_addr1).await.unwrap();
+            let listener2 = TcpListener::bind(sock_addr2).await.unwrap();
             task::spawn(async move { listener1.accept().await });
             task::spawn(async move { listener2.accept().await });
+        });
 
-            use super::Scanner;
-            let ps = Scanner::new(Duration::from_secs(4));
-
-            let ftr = ps.run_batched("127.0.0.1".to_string(), 9000, 9100, 25);
-            let my_addrs: Vec<SocketAddr> = task::block_on(async { ftr.await });
-            assert_eq!(my_addrs, vec![sock_addr1, sock_addr2]);
-            Ok(())
-        })
+        let ps = Scanner::new(Duration::from_secs(4));
+        let ftr = ps.run_batched("127.0.0.1".to_string(), 9000, 9100, 25);
+        let my_addrs: Vec<SocketAddr> = task::block_on(async { ftr.await });
+        assert_eq!(my_addrs, vec![sock_addr1, sock_addr2]);
     }
 
     #[test]
-    fn test_multiple_futures() -> io::Result<()> {
+    fn test_multiple_futures() {
+        let sock_addr1 = "127.0.0.1:5000".parse().unwrap();
+        let sock_addr2 = "127.0.0.1:5050".parse().unwrap();
+        let sock_addr3 = "127.0.0.1:5051".parse().unwrap();
+        let sock_addr4 = "127.0.0.1:5052".parse().unwrap();
+
         task::block_on(async {
-            let sock_addr1 = "127.0.0.1:5000".parse().unwrap();
-            let sock_addr2 = "127.0.0.1:5050".parse().unwrap();
-            let sock_addr3 = "127.0.0.1:5051".parse().unwrap();
-            let sock_addr4 = "127.0.0.1:5052".parse().unwrap();
-            let listener1 = TcpListener::bind(sock_addr1).await?;
-            let listener2 = TcpListener::bind(sock_addr2).await?;
-            let listener3 = TcpListener::bind(sock_addr3).await?;
-            let listener4 = TcpListener::bind(sock_addr4).await?;
+            let listener1 = TcpListener::bind(sock_addr1).await.unwrap();
+            let listener2 = TcpListener::bind(sock_addr2).await.unwrap();
+            let listener3 = TcpListener::bind(sock_addr3).await.unwrap();
+            let listener4 = TcpListener::bind(sock_addr4).await.unwrap();
             task::spawn(async move { listener1.accept().await });
             task::spawn(async move { listener2.accept().await });
             task::spawn(async move { listener3.accept().await });
             task::spawn(async move { listener4.accept().await });
+        });
 
-            use super::Scanner;
-            let ps = Scanner::new(Duration::from_secs(4));
+        let ps = Scanner::new(Duration::from_secs(4));
 
-            let ftr1 = ps.run("127.0.0.1".to_string(), 5000, 5025);
-            let ftr2 = ps.run("127.0.0.1".to_string(), 5025, 5100);
-            let all_ftrs = vec![ftr1, ftr2];
-            let my_addrs: Vec<Vec<SocketAddr>> = task::block_on(async { join_all(all_ftrs).await });
-            assert_eq!(
-                my_addrs,
-                vec![vec![sock_addr1], vec![sock_addr2, sock_addr3, sock_addr4]]
-            );
-            Ok(())
-        })
+        let ftr1 = ps.run("127.0.0.1".to_string(), 5000, 5025);
+        let ftr2 = ps.run("127.0.0.1".to_string(), 5025, 5100);
+        let all_ftrs = vec![ftr1, ftr2];
+        let my_addrs: Vec<Vec<SocketAddr>> = task::block_on(async { join_all(all_ftrs).await });
+        assert_eq!(
+            my_addrs,
+            vec![vec![sock_addr1], vec![sock_addr2, sock_addr3, sock_addr4]]
+        );
     }
 }
